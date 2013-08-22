2f5c7859d240075ee775a2e78ef7f46cb68126e1
yingliwei <yingliwei@zhihuiya.com>
2013-08-22 16:34:21 +0800

    提交测试

---
+++ study/src/main/java/com/test/lwying/JsoupTest.java
@@ -0,0 +1,35 @@
+package com.test.lwying;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * @author lwying
+ *
+ * Aug 21, 2013
+ * 
+ * Jsoup test case
+ */
+public class JsoupTest {
+    
+    public static void parseString() {
+        String html =
+            "<html><head><title>blog</title></head><body onload='test(1)'><p onload='test(2)'>Parsed HTML into a doc.</p></body></html>";
+        Document doc = Jsoup.parse(html);
+        Elements es = doc.body().getAllElements();
+        System.out.println(doc.title());
+        for (Element element : es) {
+            System.out.println(element.attr("onload"));
+        }
+    }
+    
+    /**
+     * test maia
+     * @param args
+     */
+    public static void main(String[] args) {
+        parseString();
+    }
+}
